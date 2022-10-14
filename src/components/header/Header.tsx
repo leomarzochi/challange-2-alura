294f7b5b50c43028ce25685f6bca4e19ac9d1619
Leonardo Marzochi <leomarzochi@live.com>
2022-10-14 19:18:56 -0300

    inital commit

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,13 @@
+import styles from './Header.module.scss';
+import {ReactComponent as Logo} from 'assets/logo.svg';
+import {Input} from 'components/Input/Input';
+
+export const Header = () => {
+  return (
+    <header className={styles.header}>
+      <Logo/>
+      <Input />
+
+    </header>
+  )
+}
