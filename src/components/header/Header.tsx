4346fb7fc208fa369b4ab1220ced492d1b97126c
Leonardo Marzochi <leomarzochi@live.com>
2022-10-14 20:33:47 -0300

    added components to header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,18 @@
 import styles from './Header.module.scss';
 import {ReactComponent as Logo} from 'assets/logo.svg';
 import {Input} from 'components/Input/Input';
+import {Profile} from 'components/profile/Profile';
 
 export const Header = () => {
   return (
     <header className={styles.header}>
-      <Logo/>
-      <Input placeholder='Busque por algo'/>
-
+      <div className={styles.logo}>
+        <Logo />
+      </div>
+      <div className={styles.input}>
+        <Input placeholder='Busque por algo'/>
+      </div>
+      <Profile/>
     </header>
   )
 }
