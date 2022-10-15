5136a1119dac98b52378d26185833b9ec6309f6d
Leonardo Marzochi <leomarzochi@live.com>
2022-10-15 14:51:41 -0300

    adapted sidebar

--- src/components/sidebar/menu-item/MenuItem.tsx
+++ src/components/sidebar/menu-item/MenuItem.tsx
@@ -2,19 +2,21 @@ import styles from './MenuItem.module.scss';
 
 interface Props {
   title: string,
-  icon?: string
+  icon: JSX.Element
 }
 
 export const MenuItem = ({title, icon}: Props) => {
   return (
     <div className={styles.menu}>
       <div className={styles.menu__icon_container}>
-        <i className={styles.menu__icon}>X</i>
+        <span>
+          {icon}
+        </span>
       </div>
       <div style={{'width':  '1rem'}}></div>
-      <div className={styles.menu__title}>
+      <p className={styles.menu__title}>
         {title}
-      </div>
+      </p>
     </div>
   )
 }
