59f362b1fe5c523b89929f84caac4a2240711d0a
Leonardo Marzochi <leomarzochi@live.com>
2022-10-16 20:42:01 -0300

    added media query for tablet size

--- src/screens/code-editor/CodeEditorScreen.jsx
+++ src/screens/code-editor/CodeEditorScreen.jsx
@@ -2,9 +2,9 @@ import styles from './CodeEditorScreen.module.scss';
 
 export const CodeEditorScreen = () => {
   return (
-    <>
-      <div className={styles['code-editor']}>code editor</div>
+    <div className={styles.content}>
+      <div className={styles['code-editor']}><p></p>code editor</div>
       <div className={styles['editor-options']}>aside menu</div>
-    </>
+    </div>
   )
 }
