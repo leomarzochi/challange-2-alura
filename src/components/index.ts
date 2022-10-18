50175e006bd7ee4b9327cb0d0098b619e6739481
Leonardo Marzochi <leomarzochi@live.com>
2022-10-17 22:23:11 -0300

    added menu title to barrel export

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,7 @@ export * from './avatar';
 export * from './button';
 export * from './code-editor';
 export * from './header';
+export * from './menu-title';
 export * from './profile';
 export * from './sidebar';
 export * from './text-body';
