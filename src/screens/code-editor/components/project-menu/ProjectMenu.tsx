4999c85a45d6e2d34a235bdeedeee431d406f94e
Leonardo Marzochi <leomarzochi@live.com>
2022-10-18 18:38:57 -0300

    added form container to menu

--- src/screens/code-editor/components/project-menu/ProjectMenu.tsx
+++ src/screens/code-editor/components/project-menu/ProjectMenu.tsx
@@ -1,11 +1,19 @@
-import {MenuTitle, Input} from "components";
+import {MenuTitle, Input, TextArea, Select} from "components";
 import styles from './ProjectMenu.module.scss';
 
 export const ProjectMenu = () => {
   return (
     <div className={styles.menu}>
       <MenuTitle>seu projeto</MenuTitle>
-      <Input type="text" placeholder="Nome do seu projeto"/>
+      <div className={styles['form-container']}>
+        <Input type="text" placeholder="Nome do seu projeto"/>
+        <TextArea placeholder="Descrição do seu projeto" />
+      </div>
+      <MenuTitle>Personalizacao</MenuTitle>
+      <div className={styles['form-container']}>
+        <Select />
+        <Select />
+      </div>
     </div>
   );
 }
