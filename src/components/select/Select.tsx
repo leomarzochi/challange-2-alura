77502e939251fa3067e15bd3bc2537f853ecb1d8
Leonardo Marzochi <leomarzochi@live.com>
2022-10-18 18:38:14 -0300

    created select component

---
+++ src/components/select/Select.tsx
@@ -0,0 +1,12 @@
+import styles from './Select.module.scss';
+
+export const Select = () => {
+  return (
+    <select className={styles.input}>
+      <option>oneoneoneoneoneoneoneoneoneoneoneone</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+    </select>
+  ) 
+}
