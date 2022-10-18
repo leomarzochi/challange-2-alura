3dd88d975916b7a9ebe5d661bea96036b55d1e7c
Leonardo Marzochi <leomarzochi@live.com>
2022-10-17 22:24:10 -0300

    moved menu title to its own component

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,11 +1,12 @@
 import {MenuItem} from './menu-item/MenuItem';
 import {FaCode, FaUsers} from 'react-icons/fa';
 import styles from './Sidebar.module.scss';
+import {MenuTitle} from 'components/menu-title/MenuTitle';
 
 export const Sidebar = () => {
   return (
     <aside className={styles.sidebar}>
-      <p className={styles.sidebar__title}>MENU</p>
+      <MenuTitle>MENU</MenuTitle>
       <MenuItem icon={<FaCode size={23}/>} title="Editor de código"/>
       <MenuItem icon={<FaUsers size={23}/>} title="Comunidade"/>
     </aside>
