59684cbf949650968a8ee8ea5eab377e843e27d2
Leonardo Marzochi <leomarzochi@live.com>
2022-10-19 20:21:24 -0300

    added react router link to control navigation

--- src/components/sidebar/menu-item/MenuItem.tsx
+++ src/components/sidebar/menu-item/MenuItem.tsx
@@ -1,22 +1,30 @@
 import styles from './MenuItem.module.scss';
+import {NavLink} from 'react-router-dom'
 
 interface Props {
   title: string,
-  icon: JSX.Element
+  icon: JSX.Element,
+  to: string
 }
 
-export const MenuItem = ({title, icon}: Props) => {
+export const MenuItem = ({title, icon, to}: Props) => {
   return (
     <div className={styles.menu}>
-      <div className={styles.menu__icon_container}>
-        <span>
-          {icon}
-        </span>
-      </div>
-      <div style={{'width':  '1rem'}}></div>
-      <p className={styles.menu__title}>
-        {title}
-      </p>
+      <NavLink
+        to={to}
+        className={({ isActive }) =>
+          isActive
+            ? `${styles['menu--active']} ${styles.menu__link}`
+            : styles.menu__link
+        }
+        end
+      >
+        <div className={styles.menu__icon_container}>
+          <span>{icon}</span>
+        </div>
+        <div style={{ width: '1rem' }}></div>
+        <p className={styles.menu__title}>{title}</p>
+      </NavLink>
     </div>
-  )
+  );
 }
