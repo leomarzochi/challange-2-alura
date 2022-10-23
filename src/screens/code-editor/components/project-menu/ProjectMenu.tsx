1a7c07bbff52d975641fa71de2a0cb4ae75a2660
Leonardo Marzochi <leomarzochi@live.com>
2022-10-23 18:46:00 -0300

    using new generic select

--- src/screens/code-editor/components/project-menu/ProjectMenu.tsx
+++ src/screens/code-editor/components/project-menu/ProjectMenu.tsx
@@ -1,21 +1,43 @@
-import {MenuTitle, Input, TextArea, Select, Button} from "components";
-import {ColorPicker} from "components/color-picker/ColorPicker";
+import { languageState } from 'atoms/codeEditorAtom';
+import { MenuTitle, Input, TextArea, Select, Button } from 'components';
+import { ColorPicker } from 'components/color-picker/ColorPicker';
+import { useState } from 'react';
+import { useRecoilState } from 'recoil';
 import styles from './ProjectMenu.module.scss';
 
+export interface ProjectLanguage {
+  name: string;
+  id: string;
+}
+
 export const ProjectMenu = () => {
+  const languages: readonly ProjectLanguage[] = [
+    { name: 'Javascript', id: 'javascript' },
+    { name: 'Python', id: 'python' },
+    { name: 'Java', id: 'java' },
+    { name: 'Kotlin', id: 'kotlin' },
+  ];
+
+  const [language, setLanguage] = useRecoilState(languageState);
   return (
     <div className={styles.menu}>
       <MenuTitle>seu projeto</MenuTitle>
       <div className={styles['form-container']}>
-        <Input type="text" placeholder="Nome do seu projeto"/>
+        <Input type="text" placeholder="Nome do seu projeto" />
         <TextArea placeholder="Descrição do seu projeto" />
       </div>
       <MenuTitle>Personalizacao</MenuTitle>
       <div className={styles['form-container']}>
-        <Select />
+        <Select
+          value={language}
+          options={languages}
+          onChange={setLanguage}
+          mapOptionToValue={(option: ProjectLanguage) => option.id}
+          mapOptionToLabel={(option: ProjectLanguage) => option.name}
+        />
         <ColorPicker />
         <Button buttonStyle="filled">Salvar projeto</Button>
       </div>
     </div>
   );
-}
+};
