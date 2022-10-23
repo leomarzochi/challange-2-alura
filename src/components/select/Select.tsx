ddf91a4a4939228e375b92eb74eb2dd9248398a8
Leonardo Marzochi <leomarzochi@live.com>
2022-10-23 18:45:00 -0300

    making select component more generic

--- src/components/select/Select.tsx
+++ src/components/select/Select.tsx
@@ -1,32 +1,70 @@
-import { languageState } from 'atoms/codeEditorAtom';
-import { SelectHTMLAttributes } from 'react';
-import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 import styles from './Select.module.scss';
 
-interface ProjectLanguage {
-  name: string;
-  code: string;
-}
+type Allowed = string | number;
 
-const languages: ProjectLanguage[] = [
-  { name: 'Javascript', code: 'javascript' },
-  { name: 'Python', code: 'python' },
-  { name: 'Java', code: 'java' },
-  { name: 'Kotlin', code: 'kotlin' },
-];
+type BaseProps<Value> = {
+  value: Value;
+  onChange: (newValue: Value) => void;
+  options: readonly Value[];
+  mapOptionToLabel?: (option: Value) => Allowed;
+  mapOptionToValue?: (option: Value) => Allowed;
+};
 
-export const Select = () => {
-  const setLanguage = useSetRecoilState(languageState)
-  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setLanguage(event.target.value);
+type Props<Value> = Value extends Allowed
+  ? BaseProps<Value>
+  : Required<BaseProps<Value>>;
+
+const isAllowed = (v: any): v is Allowed =>
+  typeof v === 'string' || typeof v === 'number';
+
+export function Select<Value>({
+  value,
+  onChange,
+  options,
+  mapOptionToLabel,
+  mapOptionToValue,
+}: Props<Value>) {
+  const toLabel = (option: Value): Allowed => {
+    if (mapOptionToLabel) {
+      return mapOptionToLabel(option);
+    }
+    return isAllowed(option) ? option : String(option);
   };
+
+  const toValue = (option: Value): Allowed => {
+    if (mapOptionToValue) {
+      return mapOptionToValue(option);
+    }
+    return isAllowed(option) ? option : String(option);
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    onChange(options[e.target.selectedIndex]);
+  };
+
   return (
-    <select onChange={handleSelectChange} className={styles.input}>
-      {languages.map((language) => (
-        <option key={language.code} value={language.code}>
-          {language.name}
+    <select className={styles.input} value={toValue(value)} onChange={handleChange}>
+      {options.map((value) => (
+        <option value={toValue(value)} key={toValue(value)}>
+          {toLabel(value)}
         </option>
       ))}
     </select>
   );
-};
+}
+
+// export const Select = () => {
+//   const setLanguage = useSetRecoilState(languageState)
+//   const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+//     setLanguage(event.target.value);
+//   };
+//   return (
+//     <select onChange={handleSelectChange} className={styles.input}>
+//       {languages.map((language) => (
+//         <option key={language.code} value={language.code}>
+//           {language.name}
+//         </option>
+//       ))}
+//     </select>
+//   );
+// };
