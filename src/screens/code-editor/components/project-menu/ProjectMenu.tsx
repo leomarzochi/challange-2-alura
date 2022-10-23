c9ef015eb8d2263ece5d95513517acefc431bef0
Leonardo Marzochi <leomarzochi@live.com>
2022-10-23 18:48:57 -0300

    moving type to its own file

--- src/screens/code-editor/components/project-menu/ProjectMenu.tsx
+++ src/screens/code-editor/components/project-menu/ProjectMenu.tsx
@@ -3,12 +3,10 @@ import { MenuTitle, Input, TextArea, Select, Button } from 'components';
 import { ColorPicker } from 'components/color-picker/ColorPicker';
 import { useState } from 'react';
 import { useRecoilState } from 'recoil';
+import {ProjectLanguage} from 'types';
 import styles from './ProjectMenu.module.scss';
 
-export interface ProjectLanguage {
-  name: string;
-  id: string;
-}
+
 
 export const ProjectMenu = () => {
   const languages: readonly ProjectLanguage[] = [
---
+++ src/types/shared.ts
@@ -0,0 +1,4 @@
+export interface ProjectLanguage {
+  name: string;
+  id: string;
+}
